3f901ea666300fefb39cb7b3d7454f2a1802fbda
Christian Black <56204063+CCRBlack@users.noreply.github.com>
2024-12-26 10:54:39 +0000

    Day 2, part 2.

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -15,14 +15,35 @@ if rawInput:
     safeReports = 0
     unsafeReports = 0
 
-    # For each report check it meets the "safe" criteria: all ascending or descending, and every delta is between 1 and 3 then it is safe
-    for report in reports:
-        values = [int(value) for value in report.split() if value.strip().isdigit()]
+    # Define the check function
+    def checkValues(values):
         if (sorted(values) == values or sorted(values, reverse=True) == values) \
         and all( 1<= abs(values[i] - values[i+1]) <= 3 for i in range(len(values) - 1)):
+            return True
+        else:
+            return False
+
+    # For each report check it meets the "safe" criteria: all ascending or descending, and every delta is between 1 and 3 then it is safe
+    for report in reports:
+        # Split the report into values
+        values = [int(value) for value in report.split() if value.strip().isdigit()]
+        
+        # Check the base set of values
+        if checkValues(values):
             safeReports = safeReports + 1
         else:
-            unsafeReports = unsafeReports + 1
+            # Now check if removing any value from the list will make it safe
+            for i in range(len(values)):
+                tempValues = values.copy()
+                tempValues.pop(i)
+                thisReportSafe = False
+                if checkValues(tempValues):
+                    thisReportSafe = True
+                    break
+            if thisReportSafe:
+                safeReports = safeReports + 1
+            else:
+                unsafeReports = unsafeReports + 1
 
     # Print the number of safe reports
     print("Total reports: " + str(len(reports)))
